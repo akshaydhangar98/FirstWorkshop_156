16ec71e6eb89b3ff6e9ce3eb60a28014546d7c98
akshay dhangar <akshaygdhangar14@gmail.com>
2022-06-03 23:19:11 +0530

    UC_1

---
+++ UC_1.java
@@ -0,0 +1,11 @@
+package GamblerSimulation;
+
+public class UC_1 {
+	
+	public static final int dailyStake=100;
+	public static final int bet=1;
+	
+	public static void main(String[] args) {
+		
+	}
+}
