1aaa7f5ad3ea1d759f3db2f27e7c39fd10ad34a6
akshay dhangar <akshaygdhangar14@gmail.com>
2022-06-03 23:24:56 +0530

    UC_7

---
+++ UC_7.java
@@ -0,0 +1,73 @@
+package GamblerSimulation;
+
+import java.util.Scanner;
+
+public class UC_7 {
+	
+	 public static final int dailyStake=100;
+	    public static final int bet=1;
+	    public static final int percentage=50;
+	    public static final int daysInMonth=30;
+
+	    public static void main(String[] args) {
+
+	        int day = 1;
+	        int month =1;
+	        int totalMoney=dailyStake;
+	        int unluckyDay=1;
+	        int luckyDay=1;
+	        int unluckyMonth=1;
+	        int luckyMonth=1;
+	        int unluckyDayMoney=dailyStake;
+	        int luckyDayMoney = dailyStake;
+	        int monthIncome;
+
+	        while (month<=12) {
+
+	            monthIncome=0;
+	            day=1;
+	            while (day <= daysInMonth) {
+	                UC_7 ran = new UC_7();
+	                totalMoney=dailyStake;
+	                int dailyGamePlayed = ran.nextInt(10);
+
+	                for (int i = 0; i < dailyGamePlayed; i++) {
+
+	                    if (winLoose() == 1)            //for win
+	                        totalMoney = totalMoney + 1;
+	                    else                            //for loss
+	                        totalMoney = totalMoney - 1;
+	                }
+	                day++;
+	                monthIncome=monthIncome+totalMoney;
+	            }
+	            if (monthIncome>(dailyStake*daysInMonth)) {
+	                System.out.println("You have won this month "+ month+" you have won " + monthIncome + " \nPress 1. to stop game \nPress 2. to continue");
+	                Scanner sc = new Scanner(System.in);
+	                int choice = sc.nextInt();
+	                month++;
+	                if (choice == 1)
+	                    break;
+	                else if (choice == 2)
+	                    continue;
+	            }
+	        }
+
+	    }
+
+	    public static double StakePercent(){
+	        double percentStake = percentage/100.0;
+	        return percentStake;
+	    }
+	    public static int winLoose(){
+	        UC_7 ran = new UC_7();
+	        int result = ran.nextInt(2);
+	        return result;
+	    }
+
+		private int nextInt(int i) {
+			return 0;
+		}
+
+
+}
